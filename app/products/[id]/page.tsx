785f4ebaabcf5221099648994768c00755cc79ef
Cienias <cieja0@gmail.com>
2024-07-09 18:13:58 +0200

    Change move API actions to lib/actions

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,33 +1,22 @@
-'use client';
-
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { storeTypes } from '@/type';
-import Image from 'next/image';
-import Link from 'next/link';
-const ProductDetails = () => {
-  const { id } = useParams();
-  const [product, setProduct] = useState<storeTypes>();
-
-  useEffect(() => {
-    if (id) {
-      axios
-        .get(`https://fakestoreapi.com/products/${id}`)
-        .then((response) => setProduct(response.data))
-        .catch((error) => console.error('Error fetching product:', error));
-    }
-  }, [id]);
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { storeTypes } from "@/type";
+import Image from "next/image";
+import Link from "next/link";
+import { getProductById } from "@/lib/actions/product.actions";
+const ProductDetails = async ({ params }: { params: { id: string } }) => {
+  const product = await getProductById(params.id);
 
   if (!product) return <div>Loading...</div>;
 
   return (
-    <div className='h-[100vh] flex items-center justify-center flex-col font-bold'>
+    <div className="h-[100vh] flex items-center justify-center flex-col font-bold">
       <h1>{product.title}</h1>
       <p>{product.description}</p>
       <p>${product.price}</p>
       <Image src={product.image} alt={product.title} width={100} height={100} />
-      <Link href='/products'>Back</Link>
+      <Link href="/products">Back</Link>
     </div>
   );
 };
